940c3cf1cceb5024f02506e800cbeb30ff186c69
Pronothurah <onsongojosphat28@gmail.com>
2024-04-26 04:17:07 +0300

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,55 @@
+/**
+ * Represents a  class.
+ */
+export default class Airport {
+  /**
+   * Creates a new Airport class.
+   *
+   * @param {String} code - Code of Airport.
+   * @param {String} name - Names of Airport.
+   */
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  /**
+   * Gets the name of Airport.
+   */
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * sets the name of Airport.
+   */
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /**
+   * Gets the code of airport.
+   */
+  get code() {
+    return this._code;
+  }
+
+  /**
+   * sets the code of airport.
+   */
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
