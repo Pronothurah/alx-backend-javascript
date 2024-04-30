62ba1d60b1950745ac734471669b0d2affdc64a1
Pronothurah <onsongojosphat28@gmail.com>
2024-05-01 00:28:26 +0300

    1-get_list_student_ids.js

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieves a list of student ids.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ *  }[]} students - The list of students.
+ */
+
+export default function getListStudentIds(students) {
+    if (students instanceof Array) {
+      return students.map((student) => student.id);
+    }
+    return [];
+}
